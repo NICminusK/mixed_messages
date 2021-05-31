9a2c238b49090e076103c191219bb503ea9d16f8
nicminusk <nicalvarez93@gmail.com>
2021-05-31 07:32:43 -0500

    added variables

--- mixed-messages.js
+++ mixed-messages.js
@@ -1,1 +1,7 @@
-console.log('Hello GitHub and Git and test');
+//console.log('Hello GitHub and Git and test');
+
+
+const person = ['Miss Scarlet', 'Mr. Green', 'Colonel Mustard', 'Professor Plum', 'Mrs. Peacock', 'Mrs. White'];
+const weapon = ['Candlestick', 'Dagger', 'Lead Pipe', 'Revolver', 'Rope', 'Wrench'];
+const room = ['Kitchen', 'Ball Room', 'Conservatory', 'Dining Room', 'Library', 'Billiard Room', 'Lounge', 'Hall', 'Study'];
+
