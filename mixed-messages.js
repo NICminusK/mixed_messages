33d77104ed25e2e72e6d43270fb5462ae48a0d5d
nicminusk <nicalvarez93@gmail.com>
2021-05-31 16:18:36 -0500

    generating crime

--- mixed-messages.js
+++ mixed-messages.js
@@ -5,3 +5,18 @@ const person = ['Miss Scarlet', 'Mr. Green', 'Colonel Mustard', 'Professor Plum'
 const weapon = ['Candlestick', 'Dagger', 'Lead Pipe', 'Revolver', 'Rope', 'Wrench'];
 const room = ['Kitchen', 'Ball Room', 'Conservatory', 'Dining Room', 'Library', 'Billiard Room', 'Lounge', 'Hall', 'Study'];
 
+let randomMessage = [];
+
+let randomePerson = Math.floor(Math.random() * person.length);
+let pickedPerson = person[randomePerson];
+randomMessage.push(pickedPerson);
+
+let randomeWeapon = Math.floor(Math.random() * weapon.length);
+let pickedWeapon = weapon[randomeWeapon];
+randomMessage.push(pickedWeapon);
+
+let randomRoom = Math.floor(Math.random() * room.length);
+let pickedRoom = room[randomRoom];
+randomMessage.push(pickedRoom);
+
+console.log(`Here is you generated crime: ${randomMessage.join(' ')}`);
